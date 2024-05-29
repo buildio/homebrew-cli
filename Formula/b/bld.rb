1c0608e26e6250ff3785279a297a04efb818c8e9
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2024-05-29 10:56:45 +0900

    Initial commit.

---
+++ Formula/b/bld.rb
@@ -0,0 +1,28 @@
+class Bld < Formula
+  desc "Add *bld* to your shell to interact with your Build.io applications"
+  homepage "https://build.io"
+  url "https://github.com/buildio/cli/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "d489da14768e95302c15081a628513db30361f557ca11d38a95430e2dd4edaad"
+  license "AGPL-3.0-or-later"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "libssh2" => :build
+  depends_on "crystal"
+
+  def install
+    mkdir bin
+    system "shards", "build", "--production", "--release", "--no-debug"
+    system "strip", "./bin/bld"
+    bin.install "./bin/bld"
+    ohai "----> Login to Build.io to get started."
+    ohai "      bld login"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bld --version").chomp
+  end
+end
