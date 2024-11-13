06c73316da6309d9566f91d60cc8c106e7b38d04
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2024-11-13 13:27:53 +0900

    Bumped to match v0.1.7 release.

--- Formula/b/bld.rb
+++ Formula/b/bld.rb
@@ -1,8 +1,8 @@
 class Bld < Formula
   desc "Add *bld* to your shell to interact with your Build.io applications"
   homepage "https://build.io"
-  url "https://github.com/buildio/cli/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "f0c10f137c509c38c18fb5d18aca8ed85ed8b824951d88d3f1bf85753046c8d3"
+  url "https://github.com/buildio/cli/archive/refs/tags/v0.1.7.tar.gz"
+  sha256 "f75319d32e2975083f2e4e1baabcb2160eb729bedf82d3e47aaf49140c4d8ca1"
   license "AGPL-3.0-or-later"
 
   livecheck do
